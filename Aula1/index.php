0eefd4f72b03d63707fd072f1c42c67a122b9f82
Nathann019 <natanael06aguiar@gmail.com>
2025-06-11 11:14:38 -0300

    hoje

---
+++ Aula1/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo</title>
+</head>
+
+<body>
+    <?php
+    /*
+    # Primeiro código
+    echo "Bom dia";
+    // Imprime a mensagem "Bom dia" na tela
+    /* Comentários em linhas podem ser feitos com os símbolos # e //, já os comentários com mais de uma linha devem ser feitos com os sinais /* e */
+    #phpinfo();
+
+    $texto = "Sou aluno da TI36 no Senac Botucatutututututu";
+    echo $texto;
+    
+    $divisao = "<br>==============</br>";
+    echo $divisao;
+    
+    /*
+    $nome = "chaves";
+    $sobrenome = "del Ocho";
+    echo "Olá, meu nome é $nome e meu sobrenome é $sobrenome";
+    */
+
+    # EX. 1 - Crie um script PHP que declare 3 varíaveis: $nome, $idade, e $cidade, e exiba uma mensagem que combine esses valores.
+    
+    $nome = "Alan";
+    $idade = "18";
+    $cidade = "São Paulo";
+
+    echo "Olá, me chamo $nome, tenho $idade anos e moro em $cidade!"; 
+
+    $n
+
+    ?>
+</body>
+
+</html>
